bc580c17131fda8be00a9bb77ca0e57099c0d7bb
Mirko Robbi <mirko.robbi@gmail.com>
2022-10-10 14:53:49 +0200

    finish exercise

--- js/script.js
+++ js/script.js
@@ -1,7 +1,23 @@
 
+const row = document.querySelector(".row");
+
 for (let i = 1; i < 101; i++) {
-    if (i % 15 == 0) console.log("FizzBuzz");
-    else if (i % 3 == 0) console.log("Fizz");
-    else if (i % 5 == 0) console.log("Buzz");
-    else console.log(i);
+    let fizzBuzz = "";
+    let text = i;
+    if (i % 15 == 0) {
+        console.log("FizzBuzz");
+        fizzBuzz = "fizzbuzz";
+        text = fizzBuzz;
+    } else if (i % 3 == 0) {
+        console.log("Fizz");
+        fizzBuzz = "fizz";
+        text = fizzBuzz;
+    } else if (i % 5 == 0) {
+        console.log("Buzz");
+        fizzBuzz = "buzz";
+        text = fizzBuzz;
+    }
+
+    const cell = `<div class="cell col ${fizzBuzz}">${text}</div>`;
+    row.innerHTML += cell;
 }
